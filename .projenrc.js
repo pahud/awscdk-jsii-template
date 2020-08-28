3093cc81e44ee460912658b27a804395f0274d23
Pahud <pahudnet@gmail.com>
2020-08-28 08:27:47 +0800

    feat(core): support ConstructLibraryAws

--- .projenrc.js
+++ .projenrc.js
@@ -1,41 +1,41 @@
 const {
-  JsiiProject,
-  Semver
+  ConstructLibraryAws,
 } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = '1.60.0';
-const CONSTRUCTS_VERSION = '3.0.4';
-const JSII_VERSION = '1.5.0';
+const AWS_CDK_LATEST_RELEASE = '1.61.0';
 const PROJECT_NAME = 'cdk-serverless-api';
 const PROJECT_DESCRIPTION = 'A sample JSII construct lib for AWS CDK';
 
-const project = new JsiiProject({
+const project = new ConstructLibraryAws({
   name: PROJECT_NAME,
-  jsiiVersion: Semver.caret(JSII_VERSION),
   description: PROJECT_DESCRIPTION,
   repository: 'https://github.com/pahud/awscdk-jsii-template.git',
   authorName: 'Pahud Hsieh',
   authorEmail: 'pahudnet@gmail.com',
   stability: 'experimental',
   autoReleaseSchedule: 'never',
-  devDependencies: {
-    constructs: Semver.caret(CONSTRUCTS_VERSION),
-    '@aws-cdk/assert': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@types/jest': Semver.caret('25.2.3'),
-    '@types/node': Semver.caret('14.0.11'),
+
+  keywords: [
+    'aws',
+    'fargate',
+    'autoscaler',
+  ],
+
+  catalog: {
+    twitter: 'pahudnet',
+    announce: false,
   },
-  peerDependencies: {
-    constructs: Semver.caret(CONSTRUCTS_VERSION),
-    '@aws-cdk/core': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-apigatewayv2': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-  },
-  dependencies: {
-    constructs: Semver.caret(CONSTRUCTS_VERSION),
-    '@aws-cdk/core': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-apigatewayv2': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-    '@aws-cdk/aws-lambda': Semver.pinned(AWS_CDK_LATEST_RELEASE),
-  },
+
+  // creates PRs for projen upgrades
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+
+  cdkVersion: AWS_CDK_LATEST_RELEASE,
+  cdkDependencies: [
+    '@aws-cdk/core',
+    '@aws-cdk/aws-apigatewayv2',
+    '@aws-cdk/aws-lambda',
+  ],
+
   python: {
     distName: 'cdk-serverless-api',
     module: 'cdk_serverless_api'
@@ -49,7 +49,7 @@ project.addFields({
   ]
 });
 
-const common_exclude = ['cdk.out', 'cdk.context.json', 'docker-compose.yml', 'images', 'yarn-error.log']
+const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log']
 project.npmignore.exclude(...common_exclude);
 project.gitignore.exclude(...common_exclude);
 
